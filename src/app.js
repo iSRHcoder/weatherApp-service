bda5b8fda43dd79b874b7aa52a4475ed3ff769ff
iSRHcoder <s.r.hatkadke@gmail.com>
2024-03-16 19:14:40 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import express from 'express';
 import weatherRouter from './routes/weatherRoutes.js';
 import route from './routes/routes.json' assert { type: 'json' };
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(route.ROOT, weatherRouter);
 
 export default app;
